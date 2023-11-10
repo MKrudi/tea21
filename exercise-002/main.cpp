6086736e732e710c2f826439f3589dd0849a4c18
MKrudi <149794200+MKrudi@users.noreply.github.com>
2023-11-10 10:19:37 +0100

    Adding solution 002

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -10,25 +10,32 @@ auto main(int argc, char **argv) -> int
      * CLI11 is a command line parser to add command line options
      * More information at https://github.com/CLIUtils/CLI11#usage
      */
+    // Möglichkeit, -V oder --version einzugeben, um Versionsinformationen anzuzeigen
     CLI::App app{PROJECT_NAME};
+    //Initialisierung count
+    auto count = 20;
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        //Möglichkeit, -c oder --count einzugeben, um Parameter count zu übergeben
+        app.add_option("-c,--count", count, fmt::format("Create a vector with the given size default: {}", count));
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
     {
         return app.exit(e);
     }
+    //Vektor erstellen mit Anzahl Elemente durch count vorgegeben
+    std::vector<int> data(count);
+
+    //Ausgabetext
+    fmt::print("Created a vector with {} elements", count);
 
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", app.get_name());
-
-    /* INSERT YOUR CODE HERE */
 
     return 0; /* exit gracefully*/
 }
